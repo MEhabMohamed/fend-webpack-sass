9904afe80eba76943b29749446712847282de038
MEhabMohamed <79029312+MEhabMohamed@users.noreply.github.com>
2021-04-07 00:47:15 +0200

    Applied service workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,14 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
-    optimization: {
-        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
     },
     module: {
         rules: [
@@ -20,7 +19,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
@@ -29,6 +28,6 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new MiniCssExtractPlugin({filename: '[name].css'})
+        new WorkboxPlugin.GenerateSW()
     ]
 }
