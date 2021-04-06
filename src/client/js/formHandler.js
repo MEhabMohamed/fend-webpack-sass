9f274010555e7af83c4dd57fef98414ef1f9de62
MEhabMohamed <79029312+MEhabMohamed@users.noreply.github.com>
2021-04-06 21:09:49 +0200

    Api based

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,14 +3,65 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    /*fetch('http://localhost:8080/test')
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
-    })
+    })*/
+    const performAction = async() => {
+        const name = document.getElementById('name').value;
+        const cityInfo = await getInfo();
+        try {getInfo()
+        .then(()=>{postData('/add', {name: name, city: cityInfo[0], country: cityInfo[1]})})
+        .then(()=>{getData('/gather')})
+        } catch(error) {
+          console.log('error', error)
+        }
+      };
+      performAction();
 }
-
+const getInfo = async () => {
+    const res = await fetch('https://api.ipinfodb.com/v3/ip-city/?key=1758a53a2bdb70a26a0166581f51ae76d2a527fd75ddf1a2379abf1357ca70cd&format=json')
+      try {
+          const data = await res.json();
+          const cityName = data.cityName;
+          const countryName = data.countryName;
+          console.log(data);
+          return [cityName, countryName];
+        } catch(error) {
+        console.log('error', error);
+      }
+  };
+const postData = async (url='' , data = {}) =>{
+    const response = await fetch(url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    });
+      try {
+        const newData = await response.json();
+        return newData;
+      } catch(error) {
+        console.log("error", error);
+      }
+  };
+  const getData = async (url) => {
+    const respond = await fetch(url);
+    try {
+      const data = await respond.json();
+      const name = data.name.toUpperCase();
+      const city = data.city;
+      const country = data.country;
+      document.getElementById('results').textContent = (`Welcome ${name} from ${city}, ${country}!`);
+    }
+    catch (error) {
+      console.log("error", error);
+    }
+  };
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,18 @@
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+/*const mockAPIResponse = require('./mockAPI.js')*/
+projectData = {};
 
 const app = express()
 
 app.use(express.static('dist'))
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
 
 console.log(__dirname)
 
@@ -17,6 +25,12 @@ app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+app.get('/gather', function (req, res) {
+    res.send(projectData)
+})
+
+app.post('/add', function(req, res) {
+    projectData.name = req.body.name;
+    projectData.city = req.body.city;
+    projectData.country = req.body.country;
 })
